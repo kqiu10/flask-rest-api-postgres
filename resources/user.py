ff07c16b70e3ab244b738b4838e73fee69718a21
kqiu10 <qiukan97@gmail.com>
2021-01-18 11:36:37 -0500

    added jwt-extended

--- resources/user.py
+++ resources/user.py
@@ -2,21 +2,24 @@ from flask_restful import Resource, reqparse
 from models.user import UserModel
 
 
+_user_parser = reqparse.RequestParser()
+
+_user_parser.add_argument('username',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank."
+                    )
+_user_parser.add_argument('password',
+                    type=str,
+                    required=True,
+                    help="This field cannot be blank."
+                    )
+
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
-                        )
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
-                        )
+
 
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
@@ -28,6 +31,8 @@ class UserRegister(Resource):
 
 
 class User(Resource):
+
+
     @classmethod
     def get(cls, user_id):
         user = UserModel.find_by_id(user_id)
@@ -42,3 +47,5 @@ class User(Resource):
             return {'message': 'User not found'}, 404
         user.delete_from_db()
         return {'message': 'User deleted.'}, 200
+
+class UserLogin(Resource):
