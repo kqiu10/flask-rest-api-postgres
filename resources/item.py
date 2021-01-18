5c2e4b660acdfe8a1e713e9ebfb279d54b705919
kqiu10 <qiukan97@gmail.com>
2021-01-18 13:40:41 -0500

    added token need for posting new item

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,9 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims, get_jwt_identity, jwt_optional
+from flask_jwt_extended import (jwt_required,
+                                get_jwt_claims,
+                                get_jwt_identity,
+                                jwt_optional,
+                                fresh_jwt_required)
 from models.item import ItemModel
 
 
@@ -23,6 +27,7 @@ class Item(Resource):
             return item.json()
         return {'message': 'Item not found'}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
